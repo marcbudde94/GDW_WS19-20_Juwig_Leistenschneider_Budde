3f06c465d652fb131945b4f8b3528f54470df1b2
nleisten <32449628+nleisten@users.noreply.github.com>
2020-01-20 13:52:50 +0100

    Update Rest Code

--- REST- Code/api/routes/users.js
+++ REST- Code/api/routes/users.js
@@ -6,6 +6,7 @@ var request = require("request");
 
 const User = require("../models/user");
 const Lebensmittel = require("../models/lebensmittel");
+const { ResourceNotFoundError, InternalError } = require('./errors.js');
 
 router.get("/", (req, res, next) => {
     User.find()
@@ -119,36 +120,90 @@ router.get("/:userID/lebensmittel", (req, res, next) => {
 });
 
 router.post("/:userID/lebensmittel", (req, res, next) => {
+        var protein;
+        var kcal;
+        var kohlenhydrate;
+        var fett;
+        var options = {
+            method: 'GET',
+            url: 'https://api.edamam.com/api/food-database/parser',
+            qs:
+                {
+                    app_id: '9a036e31',
+                    app_key: '34cc33028da034f70dfeaa37342b2063',
+                    'nutrition-type': 'logging',
+                    ingr: req.body.name,
+                    category: 'Generic%20foods'
+                },
+            headers:
+                {
+                    'cache-control': 'no-cache',
+                    'Content-Type': 'application/json'
+                }
+        };
 
 
-    const lebensmittel = new Lebensmittel({
-        _id: new mongoose.Types.ObjectId(),
-        name: req.body.name,
-        istAbzugeben: req.body.istAbzugeben,
-        reserviertVon: req.body.reserviertVon,
-        protein: req.body.protein,
-        fett: req.body.fett,
-        kohlenhydrate: req.body.kohlenhydrate,
-        kcal: req.body.kcal
-    });
-    lebensmittel
-        .save()
-        .then(result => {
-            console.log(result);
-            res.status(201).json({
-                message: "Handling POST requests to /user/userID/lebensmittel",
-                createdLebensmittel: result
+        new Promise
+        ((resolve, reject) => {
+            request(options, function (err, response, body) {
+                if (err) {
+                    reject(err);
+                }
+                else {
+                    let food = body;
+                    resolve(food);
+                }
             });
         })
-        .then(result => {
+            .then(food => {
+                food = JSON.parse(food);
+                if (food.parsed.length == 0) { // Falls es keine Ergebnisse zum Angegebenen Lebensmittel gibt wird Fehler geworfen
+                    throw err = new ResourceNotFoundError('Lebensmittel', options.qs.ingr);
+                }
 
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json({
-                error: err
+                protein = food.parsed[0].food.nutrients.PROCNT;
+                fett = food.parsed[0].food.nutrients.FAT;
+                kohlenhydrate = food.parsed[0].food.nutrients.CHOCDF;
+                kcal = food.parsed[0].food.nutrients.ENERC_KCAL;
+
+            })
+            .then(food =>{
+                const lebensmittel = new Lebensmittel({
+                    _id: new mongoose.Types.ObjectId(),
+                    name: req.body.name,
+                    istAbzugeben: req.body.istAbzugeben,
+                    reserviertVon: req.body.reserviertVon,
+                    protein: protein,
+                    fett: fett,
+                    kohlenhydrate: kohlenhydrate,
+                    kcal: kcal
+                });
+                lebensmittel
+                    .save()
+                    .then(result => {
+                        console.log(result);
+                        res.status(201).json({
+                            message: "Handling POST requests to /user/userID/lebensmittel",
+                            createdLebensmittel: result
+                        });
+                    })
+                    .then(result => {
+
+                    })
+                    .catch(err => {
+                        console.log(err);
+                        res.status(500).json({
+                            error: err
+                        });
+                    });
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(500).json({
+                    error: err
+                });
             });
-        });
+
 });
 
 router.get("/:userId/lebensmittel/:lebensmittelID", (req, res, next) => {
